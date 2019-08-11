f9d737403747dbe0de35fec36fe22bb1097183ec
ShitProgrammer <44732231+ShitProgrammer@users.noreply.github.com>
2019-08-11 14:59:51 -0500

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+
+window.onscroll = function() {StickyNav()};
+
+function StickyNav() {
+
+	var navbar = document.getElementById("navbar");
+	var sticky = navbar.offsetTop;
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
